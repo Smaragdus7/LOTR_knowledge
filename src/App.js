91a8f2f584da4695d91c917c2287cd817a3c8e57
Smaragdus7 <smar.leeft@gmail.com>
2021-07-21 09:36:44 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+
+import { LoginButton } from './Login';
+import { LogoutButton } from './Logout';
+import { Profile } from './Profile';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>Login here</p>
+      {isAuthenticated ? <>
+        <Profile />
+        <LogoutButton />
+        </>
+        :
+        <LoginButton />
+      }
       </header>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain="dev-3mga0ky3.us.auth0.com" clientId="OsjaQrBhRUSXn2FMua3vTHXy0Eq2jh3i" redirectUri="window.location.origin"><App /></Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
